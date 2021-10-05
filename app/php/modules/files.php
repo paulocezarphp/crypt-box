27dcb0faf500467473dee99678da5b6688c9c8a7
Paulo <cezarleigos28@outlook.com>
2021-10-05 09:32:39 -0300

    v0.03 -  Finishing structures
    This time we finalize the structures that we will use in our application, we start creating the files that we will use to perform all the operations that we will use in our project.

---
+++ app/php/modules/files.php
@@ -0,0 +1,11 @@
+<?php
+   
+   function save_file(){
+
+   }
+
+   function open_file(){
+
+   }
+
+?>
--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
 <head>
 	<title>Crypt Box</title>
+	<link rel="stylesheet" type="text/css" href="app/css/style.css">
+	<link rel="stylesheet" type="text/css" href="app/css/style-mobile.css">
+	<link rel="stylesheet" type="text/css" href="app/css/style-tablet.css">
+	<link rel="stylesheet" type="text/css" href="app/css/style-desktop.css">
 </head>
 <body>
 
   
+    <script type="text/javascript" src="app/js/script.js"></script>
 </body>
 </html>
